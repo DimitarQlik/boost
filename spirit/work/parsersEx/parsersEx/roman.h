b7649622e0607e014a8e2203c59d131649138d42
Dimitar Gueorguiev <due@qlik.com>
2016-02-26 21:31:54 -0500

    some refactoring and fixing an issue in the roman parser 2-26-16

---
+++ spirit/work/parsersEx/parsersEx/roman.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <boost/config/warning_disable.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+
+#include <iostream>
+#include <string>
+
+namespace client
+{
+	
+
+	bool test_roman();
+}
